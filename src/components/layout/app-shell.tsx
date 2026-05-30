0c3b5efd9069c18ab6e311c3c6ba4f417b3db1fd
yeonhee7935 <yeonhee7935@naver.com>
2026-05-30 10:46:08 +0900

    style: 예약 완료 페이지 전체 높이 차지하도록 수정 및 레이아웃 개선

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -1,28 +1,41 @@
-import * as React from "react"
-import { ConditionalNav } from "./conditional-nav"
-import { cn } from "@/lib/utils"
+'use client';
+
+import * as React from 'react';
+import { usePathname } from 'next/navigation';
+import { ConditionalNav } from './conditional-nav';
+import { cn } from '@/lib/utils';
+import { shouldHideBottomNav } from '@/lib/nav-utils';
 
 interface AppShellProps {
-  children: React.ReactNode
-  className?: string
-  hideNav?: boolean
+  children: React.ReactNode;
+  className?: string;
+  hideNav?: boolean;
 }
 
-function AppShell({ children, className, hideNav = false }: AppShellProps) {
+function AppShell({
+  children,
+  className,
+  hideNav: hideNavProp = false,
+}: AppShellProps) {
+  const pathname = usePathname();
+
+  // hideNavProp이 true이거나, 현재 경로가 내비게이션을 숨겨야 하는 경로인 경우
+  const isNavHidden = hideNavProp || shouldHideBottomNav(pathname);
+
   return (
-    <div className="flex justify-center min-h-dvh bg-(--color-neutral-150)">
+    <div className="flex min-h-dvh justify-center bg-(--color-neutral-150)">
       <div
         className={cn(
-          "relative flex flex-col w-full max-w-107.5 min-h-dvh bg-(--color-neutral-150)",
-          !hideNav && "pb-20",
+          'relative flex min-h-dvh w-full max-w-107.5 flex-col bg-(--color-neutral-150)',
+          !isNavHidden && 'pb-20',
           className
         )}
       >
         {children}
-        {!hideNav && <ConditionalNav />}
+        {!isNavHidden && <ConditionalNav />}
       </div>
     </div>
-  )
+  );
 }
 
-export { AppShell }
+export { AppShell };
