b02c1905ad09a075aa2bb954d69d9bb4250d0f5c
yeonhee7935 <yeonhee7935@naver.com>
2026-06-01 02:46:35 +0900

    feat: 스플래시 화면 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,91 @@ export const metadata: Metadata = {
   title: '샘깊은교회 장소 예약',
   description: '샘깊은교회 장소 예약 서비스',
   manifest: '/manifest.json',
-  colorScheme: 'light',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: '문사방',
+    startupImage: [
+      {
+        url: '/pwa/apple-splash-2048-2732.png',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1668-2388.png',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1536-2048.png',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1668-2224.png',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1620-2160.png',
+        media:
+          '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1290-2796.png',
+        media:
+          '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1179-2556.png',
+        media:
+          '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1284-2778.png',
+        media:
+          '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1170-2532.png',
+        media:
+          '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1125-2436.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1242-2688.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-828-1792.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-1242-2208.png',
+        media:
+          '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-750-1334.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      {
+        url: '/pwa/apple-splash-640-1136.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+    ],
+  },
+  other: {
+    'apple-touch-icon': '/pwa/apple-icon-180.png',
+  },
 };
 
 export const viewport: Viewport = {
